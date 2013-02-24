fbed05910da150fead92c105374f3d8ad66aba91
drjerm <utilityjerm@gmail.com>
2013-02-24 16:51:03 -0700

    started work with three.js scene

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -25,7 +25,12 @@ buildPortfolio = function(){
 }
 
 var scene, camera, renderer;
+var cubes = [];
+var backRow = [];
+var frontRow = [];
 var size = 10;
+var xGrid = 6;
+var zGrid = 2;
 
 init = function(){
 
@@ -35,15 +40,15 @@ init = function(){
     scene = new THREE.Scene();
 
     camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 2000 );
-    camera.position.x = 100;
-    camera.position.y = 100;
-    camera.position.z = 0;
-    camera.lookAt( new THREE.Vector3( 0, - 50, 0 ) );
+    camera.position.x = xGrid*size/2+5;
+    camera.position.y = 10;
+    camera.position.z = 65;
+    camera.lookAt( new THREE.Vector3( xGrid*size/2+5, 10, 0 ) );
     scene.add( camera );
 
-//    scene.add( new THREE.AmbientLight( 0x808080 ) );
+    scene.add( new THREE.AmbientLight( 0x808080 ) );
 
-    light = new THREE.SpotLight( 0xffffff, 1.25 );
+    light = new THREE.SpotLight( 0xFAFAFA, 1 );
     light.position.set( - 500, 900, 600 );
     light.target.position.set( 0, 0, 0 );
     light.castShadow = true;
@@ -51,13 +56,12 @@ init = function(){
 
     var geometry = new THREE.CubeGeometry( size, size, size );
     //geometry.applyMatrix( new THREE.Matrix4().setTranslation( 0, size / 2, 0 ) );
-    // var material = new THREE.MeshLambertMaterial( { color: 0xd0d0d0 } );
-    var material = new THREE.MeshLambertMaterial( { color: 0xffd0d0 } );
+    var material = new THREE.MeshLambertMaterial( { color: 0xd0d0d0 } );
+    //var material = new THREE.MeshLambertMaterial( { color: 0xffd0d0 } );
 
-    var xGrid = 6;
-    var zGrid = 2;
+    
 
-    var cubes = [];
+    
 
     for ( var x = 0; x < xGrid; x++ ) {
         for( var z = 0; z < zGrid; z++){
@@ -69,21 +73,27 @@ init = function(){
             cube.receiveShadow = true;
             scene.add( cube );
 
+            if(z== 0)
+                backRow.push(cube);
+            else
+                frontRow.push(cube);
             cubes.push( cube );
         }
 
     }
 
-    // geometry = new THREE.PlaneGeometry( sizeres, sizeres );
+    geometry = new THREE.PlaneGeometry( 500, 500 );
 
-    // plane = new THREE.Mesh( geometry, material );
-    // plane.position.x = halfsizeres;
-    // plane.position.z = halfsizeres;
-    // plane.rotation.x = - 90 * Math.PI / 180;
+    plane = new THREE.Mesh( geometry, material );
+    plane.position.x = 0;
+    plane.position.y = size/2;
+    plane.rotation.x = - 90 * Math.PI / 180;
     // plane.visible = false;
-    // scene.add( plane );
+    scene.add( plane );
 
     renderer = new THREE.WebGLRenderer();
+
+    renderer.setClearColorHex( 0x202020, 1 );
 
     renderer.shadowMapEnabled = true;
     renderer.shadowMapSoft = true;
@@ -100,19 +110,34 @@ init = function(){
     var devicePixelRatio = window.devicePixelRatio || 1;
 
     renderer.setSize( window.innerWidth * devicePixelRatio, window.innerHeight * devicePixelRatio );
-    renderer.domElement.style.width =960 + 'px';
+    renderer.domElement.style.width =window.innerWidth + 'px';
     renderer.domElement.style.height = 430 + 'px';
 
     canvas.appendChild( renderer.domElement );
 
 
+    window.addEventListener( 'resize', onWindowResize, false );
+}
+onWindowResize = function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight ); 
+    renderer.domElement.style.width =window.innerWidth + 'px';
+    renderer.domElement.style.height = 430 + 'px';
 }
 
 
 animate = function() {
 
     requestAnimationFrame( animate );
-
+    for (i in backRow){
+        var cube = backRow[i];
+        cube.position.y += .05;
+    }
+    for (i in frontRow){
+        var cube = frontRow[i];
+        cube.position.y += .01;
+    }
     render();
 }
 
